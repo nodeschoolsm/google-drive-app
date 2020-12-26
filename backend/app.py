6b7e24b3a4bb1c51a6a9cbf81c2ee4237f2e9221
Nelson-developer <nelsonher019@gmail.com>
2020-12-25 20:50:42 -0600

    Backend de bobby dockerizado

--- backend/app.py
+++ backend/app.py
@@ -2,10 +2,17 @@ from flask import Flask
 from queries import queries_graphql
 from endpoints import endpoints_flask
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
 
+#MIDDLEWARE QUE CREA EL FOLDER IMAGES SI NO EXISTE
+@app.before_first_request
+def create_folder():
+    if os.path.exists(os.getcwd() + "/files") != True or os.path.exists(os.getcwd() + "/papelera"):
+        os.makedirs(os.getcwd() + "/files")
+        os.makedirs(os.getcwd() + "/papelera")
 
 
 if __name__ == "__main__":
